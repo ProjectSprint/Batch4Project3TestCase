fa6c8f32fe0dd2b53123d66621daccd7d1cce177
prasasdi <rizky.adie7@gmail.com>
2025-10-03 13:36:20 +0700

    skenario GET item dan merchant test

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,12 +1,16 @@
 import type z from "zod";
 import { AuthSchema } from "./auth.schema";
 import { UserSchema } from "./user.schema";
+import { MerchantSchema } from "./merchant.schema";
+import { ItemSchema } from "./item.schema";
 
 // biome-ignore lint/suspicious/noExplicitAny: we can't determine what ObjectSchema output will be
 type ObjectSchema = z.ZodType<Record<string, unknown>, any, any>;
 type SchemaRegistry = Record<string, ObjectSchema>;
 
 export const schemaRegistry: SchemaRegistry = {
+	item: ItemSchema,
+  merchant: MerchantSchema,
 	user: UserSchema,
 	auth: AuthSchema,
 };
